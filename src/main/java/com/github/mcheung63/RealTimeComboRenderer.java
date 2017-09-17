77c190050923ebcf148f7c1acaf78484a0bf49a0
Peter <mcheung63@hotmail.com>
2017-09-16 22:07:52 -0700

    dev

--- src/main/java/com/github/mcheung63/RealTimeComboRenderer.java
+++ src/main/java/com/github/mcheung63/RealTimeComboRenderer.java
@@ -2,6 +2,7 @@ package com.github.mcheung63;
 
 import java.awt.Component;
 import java.io.File;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
@@ -10,15 +11,19 @@ import javax.swing.ListCellRenderer;
  *
  * @author Peter <peter@quantr.hk>
  */
-public class RealTimeComboRenderer extends JLabel implements ListCellRenderer {
+public class RealTimeComboRenderer implements ListCellRenderer {
+
+	DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
 
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+		JLabel label = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index,
+				isSelected, cellHasFocus);
 		File file = (File) value;
 		if (file != null) {
-			setText(file.getName());
+			label.setText(file.getName());
 		}
-		return this;
+		return label;
 	}
 
 }
